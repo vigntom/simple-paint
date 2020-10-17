90f0d20f3c5b1c4a4350640d3dac63f2de33cff4
Aleksandr Ignatev <vigntom@gmail.com>
2020-10-18 00:30:53 +0300

    Animate point appearance

--- src/components/canvas/draw.js
+++ src/components/canvas/draw.js
@@ -1,11 +1,34 @@
-function point (ctx, point) {
-  const pointSize = 10
+function createPoint (ctx, point, pointSize) {
   const color = '#ff2626'
 
   ctx.fillStyle = color
   ctx.beginPath()
   ctx.arc(...point, pointSize, 0, Math.PI * 2, true)
   ctx.fill()
+}
+
+function point (ctx, point) {
+  const pointSize = 10
+  const duration = 300
+  const startTime = new Date()
+  let animationFrameId
+
+  function render () {
+    const currentTime = new Date()
+    const delta = currentTime - startTime
+
+    if (delta >= duration) {
+      return window.cancelAnimationFrame(animationFrameId)
+    }
+
+    const size = delta * pointSize / duration
+
+    createPoint(ctx, point, size)
+
+    animationFrameId = window.requestAnimationFrame(render)
+  }
+
+  render()
 }
 
 function line (ctx, point1, point2) {
